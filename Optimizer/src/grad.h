580946e023d72a6c3635b9f18875631fd99c2e88
pirate <diptanshu@iitg.ernet.in>
2018-10-07 17:49:36 +0530

    Added Gradient Function and DFP Algorithm.
    Added Variable-Metric Method, a multi-variable optimization algorithm
    and a function to calculate gradients using central difference.

--- Optimizer/src/grad.h
+++ Optimizer/src/grad.h
@@ -6,7 +6,7 @@
 #define OPTIMIZE_GRAD_H
 #endif
 
-Eigen::Vector2d derivative (double (*func)(double), double x) {
+Eigen::Vector2d Derivative (std::function<double (double)> func, double x) {
     double delta = 1e-5;
     double fx1 = func(x + delta);
     double fx2 = func(x - delta);
@@ -18,4 +18,17 @@ Eigen::Vector2d derivative (double (*func)(double), double x) {
     res << a, b;
 
     return res;
+}
+
+Eigen::VectorXd Gradient (std::function<double (Eigen::VectorXd)> func, Eigen::VectorXd x) {
+    int n = x.size();
+    Eigen::VectorXd G(n), delta_xi = Eigen::VectorXd::Zero(n);
+    double delta = 1e-5;
+    for (int i = 0; i < n; ++i) {
+        delta_xi(i) = delta;
+        G(i) = (func(x + delta_xi) - func(x - delta_xi)) / (2 * delta);
+        delta_xi(i) = 0;
+    }
+
+    return G;
 }
--- Optimizer/src/optimizer.h
+++ Optimizer/src/optimizer.h
@@ -8,7 +8,7 @@
 #endif
 
 
-Eigen::Vector2d boundingPhase (double (*obj_func)(double), double x) {
+Eigen::Vector2d BoundingPhase (std::function<double (double)> obj_func, double x) {
     double delta = 0.5;
     int k = 0, M = 500;
     double fx1 = obj_func(x + delta), fx2 = obj_func(x), fx3 = obj_func(x - delta);
@@ -36,17 +36,60 @@ Eigen::Vector2d boundingPhase (double (*obj_func)(double), double x) {
     return res;
 }
 
-double newtonRapshon (double (*obj_func)(double), Eigen::Vector2d range) {
+double NewtonRapshon (std::function<double (double)> obj_func, Eigen::Vector2d range) {
     double x = (range(0) + range(1)) / 2;
     double epsilon = 1e-5;
     int it = 1, M = 500;
 
-    Eigen::Vector2d dfx = derivative(obj_func, x);
+    Eigen::Vector2d dfx = Derivative(obj_func, x);
 
     while (std::abs(dfx(0)) > epsilon && dfx(1) != 0 && it < M) {
         x -= dfx(0) / dfx(1);
-        dfx = derivative(obj_func, x);
+        dfx = Derivative(obj_func, x);
         ++it;
     }
+    return x;
+}
+
+double SVOptimize (std::function<double (double)> obj_func, double x) {
+    Eigen::Vector2d range = BoundingPhase(obj_func, x);
+    return NewtonRapshon(obj_func, range);
+}
+
+Eigen::VectorXd DFP (std::function<double (Eigen::VectorXd)> obj_func, Eigen::VectorXd x) {
+    int n = x.size();
+    int it = 0, M = 1000;
+    double epsilon = 1e-5;
+
+    Eigen::VectorXd x_prev = x;
+    Eigen::VectorXd G = Gradient(obj_func, x), G_prev = G;
+    if (G.norm() < epsilon)
+        return x;
+
+    Eigen::MatrixXd A = Eigen::MatrixXd::Identity(n, n);
+    Eigen::VectorXd S = -G;
+
+    while (it < M) {
+        std::function<double (double)> func = [obj_func, x, S] (double a)->double { return obj_func(x + a * S); };
+        double alpha = SVOptimize(func, 0.0);
+        x_prev = x;
+        x += alpha * S;
+        ++it;
+
+        if ((x - x_prev).norm() / x_prev.norm() < epsilon)
+            break;
+
+        G_prev = G;
+        G = Gradient(obj_func, x);
+        if (G.norm() < epsilon)
+            break;
+
+        Eigen::VectorXd delta_x = x - x_prev;
+        Eigen::VectorXd delta_G = G - G_prev;
+        A = A + (delta_x * delta_x.transpose())/(delta_x.transpose() * delta_G) - (A * delta_G * (A * delta_G).transpose())/(delta_G.transpose() * A * delta_G);
+
+        S = -A * G;
+    }
+
     return x;
 }
