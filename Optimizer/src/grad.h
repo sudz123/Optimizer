64e2574c608b27e77a15d7122d58eec5f317b411
Sudarshan Kamath <sudarshan.kamath97@gmail.com>
2018-10-07 15:46:03 +0530

    Comments to grad.h

--- Optimizer/src/grad.h
+++ Optimizer/src/grad.h
@@ -1,12 +1,19 @@
+% Macro for Eigen
 #ifndef EIGEN_MATRIX_H
 #include <Eigen/Dense>
 #endif
 
+% Macro for Optimizer
 #ifndef OPTIMIZE_GRAD_H
 #define OPTIMIZE_GRAD_H
 #endif
 
+% Function to calculate derivative using central differencing
+% Inputs are a function pointer of type Eigen::obj_func and a double variable
+% wrt which the function is being derivated
+% Returns an Eigen::Vector2d
 Eigen::Vector2d derivative (double (*func)(double), double x) {
+    
     double delta = 1e-5;
     double fx1 = func(x + delta);
     double fx2 = func(x - delta);
