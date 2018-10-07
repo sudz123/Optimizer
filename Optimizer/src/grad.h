025871e3418eae2cc60cbaac0f972d3edb628cf0
Sudarshan Kamath <sudarshan.kamath97@gmail.com>
2018-10-07 15:49:19 +0530

    Update grad.h

--- Optimizer/src/grad.h
+++ Optimizer/src/grad.h
@@ -9,7 +9,7 @@
 #endif
 
 // Function to calculate derivative using central differencing
-// Inputs are a function pointer of type Eigen::obj_func and a double variable
+// Inputs are a function pointer and a double variable
 // wrt which the function is being derivated
 // Returns an Eigen::Vector2d
 Eigen::Vector2d derivative (double (*func)(double), double x) {
