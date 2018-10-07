6d53b9ec4eb00cd453e0055b765ed616803a9dd1
Sudarshan Kamath <sudarshan.kamath97@gmail.com>
2018-10-07 16:06:09 +0530

    Update grad.h

--- Optimizer/src/grad.h
+++ Optimizer/src/grad.h
@@ -10,7 +10,7 @@
 
 // Function to calculate derivative using central differencing
 // Inputs are a function pointer and a double variable
-// wrt which the function is being derivated
+// This variable holds the value at which we have to apply central differencing
 // Returns an Eigen::Vector2d
 Eigen::Vector2d derivative (double (*func)(double), double x) {
     
