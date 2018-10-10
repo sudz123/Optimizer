935a4f1e0ac70923357d2ae9627a03ee733c79d7
Mado <pchlad5@gmail.com>
2018-10-10 22:49:31 +0200

    Added functionality to SaveAsTxt() and SaveAsCsv() Optimum class methods

--- Optimizer/src/util.h
+++ Optimizer/src/util.h
@@ -8,9 +8,26 @@
 #define OPTIMIZE_UTIL_H
 #endif
 
+//Libs for file operations
+#include <iostream>
+#include <fstream>
+
+//ISSUE #76
+/*util.h has two unimplemented functions in the class Optimum
+
+    SaveAsTxt()
+    SaveAsCsv()
+
+On being called these should respectively create a txt/csv file. 
+These files should have each line storing data from one row of Eigen::MatrixXd path.
+ Should be fairly easy.*/
+
+
+
 namespace Optimizer {
 
-	class Optimum{
+	class Optimum
+    {
 
     public:
         Eigen::VectorXd optima;
@@ -19,13 +36,52 @@ namespace Optimizer {
         std::string term_cond;
         int it;
         int func_eval;
-
-        void SaveAsTxt(){
-
+     
+        void SaveAsTxt()
+        {
+               //NOTE: REMOVE PATH BEFORE PULL REQUEST
+            //create new file
+            std::ofstream file_stream("data.txt"); 
+            if(file_stream.is_open() == true)
+            {
+                //go through entire matrix
+                for(int i = 0; i < path.cols(); i++)
+                {
+                    for(int j = 0; j < path.rows(); j++)
+                    {
+                        //print out whole row
+                        file_stream << path(i,j) << " ";
+                    }
+                    //after every row add new line
+                    file_stream << std::endl;
+                }
+            }
         }
 
-        void SaveAsCsv(){
+        void SaveAsCsv()
+        {
+            //create new file
+            std::ofstream file_stream("data.csv");
+            if(file_stream.is_open() == true)
+            {
+                //go through entire matrix
+               for(int i = 0; i < path.cols(); i++)
+                {
+                    for(int j = 0; j < path.rows(); j++)
+                    {
+                        file_stream << path(i,j);
+                        //add comma after each coefficient in matrix
+                        //all but not last
+                        if(j < path.rows()-1)
+                        {
+                            file_stream << ',';
+                        }
+                    }
+                    //add carrige return after every row
+                    file_stream << '\r' << '\n';
+                }
 
+            }
         }
 
     };
