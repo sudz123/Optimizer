92df1f5697ec1c1bd3ff822bf0b5c7ba25586e1c
Debodirno Chandra <debodirno.chandra@bloomreach.com>
2018-10-10 22:45:55 +0530

    Renamed test functions to meaningful ones - func2 to SumSquares - func3 to Matyas

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -11,11 +11,11 @@ double func (double x) {
     return pow(x + 10, 2);
 }
 
-double func2 (VectorXd x) {
+double SumSquares (VectorXd x) {
     return x.squaredNorm();
 }
 
-double func3 (Vector2d x) {
+double Matyas (Vector2d x) {
     return 0.26 * (pow(x(0),2) + pow(x(1),2)) - 0.48 * x(0) * x(1);
 }
 
@@ -43,36 +43,36 @@ int main () {
     cout << "The Optimal Point obtained is: ";
     cout << SVOptimize(func, 5.4) << endl;
 
-    cout << "Testing Gradient function using func2 with 3 variables" << endl;
-    cout << Gradient(func2, Vector3d(3, 3, 4)) << endl;
+    cout << "Testing Gradient function using SumSquares with 3 variables" << endl;
+    cout << Gradient(SumSquares, Vector3d(3, 3, 4)) << endl;
 
-    cout << "Testing Hessian function using func2 with 3 variables" << endl;
-    cout << Hessian(func2, Vector3d(3, 3, 4)) << endl;
+    cout << "Testing Hessian function using SumSquares with 3 variables" << endl;
+    cout << Hessian(SumSquares, Vector3d(3, 3, 4)) << endl;
 
     Vector3d x(4, -3, 7);
 
     cout << "Testing DFP on sum squared function with 3 variables and initial point (4, -3, 7)." << endl;
     cout << "The Optimal Point obtained is: " << endl;
-    cout << DFP(func2, x) << endl;
+    cout << DFP(SumSquares, x) << endl;
 
     cout << "Testing Newton's method on sum squared function with 3 variables and initial point (4, -3, 7)." << endl;
     cout << "The Optimal Point obtained is: " << endl;
-    cout << Newton(func2, x) << endl;
+    cout << Newton(SumSquares, x) << endl;
 
     cout << "Testing Cauchy's method on sum squared function with 3 variables and initial point (4, -3, 7)." << endl;
     cout << "The Optimal Point obtained is: " << endl;
-    cout << Cauchy(func2, x) << endl;
+    cout << Cauchy(SumSquares, x) << endl;
 
     cout << "Testing Marquardt's method on sum squared function with 3 variables and initial point (4, -3, 7)." << endl;
     cout << "The Optimal Point obtained is: " << endl;
-    cout << Marquardt(func2, x) << endl;
+    cout << Marquardt(SumSquares, x) << endl;
 
     cout << "Testing Conjugate Gradient method on sum squared function with 3 variables and initial point (4, -3, 7)." << endl;
     cout << "The Optimal Point obtained is: " << endl;
-    cout << ConjugateGradient(func2, x) << endl;
+    cout << ConjugateGradient(SumSquares, x) << endl;
 
     cout << "Testing Conjugate Gradient method on Matyas function with 2 variables and initial point (-3, 7)." << endl;
     cout << "The Optimal Point obtained is: " << endl;
-    cout << ConjugateGradient(func3, Vector2d(-3, 7)) << endl;
+    cout << ConjugateGradient(Matyas, Vector2d(-3, 7)) << endl;
 
 }
