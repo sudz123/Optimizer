c6241febdbd59b912d147f6ce2948355d6214c9f
Sudarshan Kamath <sudarshan.kamath97@gmail.com>
2018-10-20 21:27:41 +0530

    Update optimizer.h

--- Optimizer/src/optimizer.h
+++ Optimizer/src/optimizer.h
@@ -857,30 +857,21 @@ namespace Optimizer {
         return ans;
     }
 
-<<<<<<< Sudz-Image
-    double GetPenalty(double R, Eigen::VectorXd x, std::vector<std::function<double (Eigen::VectorXd)>> constraints, PF p_func = PF::BRACKET){
-        
-        Eigen::VectorXd temp = Eigen::VectorXd::Zero(constraints.size());
+	double GetPenalty (std::vector<std::function<double (Eigen::VectorXd)>> constraints, Eigen::VectorXd x, PF penalty = PF::BRACKET) {
+		Eigen::VectorXd temp = Eigen::VectorXd::Zero(constraints.size());
+		switch (penalty) {
+			case PF::BRACKET: return Bracket(constraints, x, temp);
+			case PF::PARABOLIC: return Parabolic(constraints, x, temp);
+			case PF::LOG: return Log(constraints, x);
+			case PF::INVERSE: return Inverse(constraints, x);
+			case PF::INF_BARRIER: return InfiniteBarrier(constraints, x);
+			default: return Bracket(constraints, x, temp);
+		}
+	}
 
-        switch (p_func) {
-
-            case PF::PARABOLIC: return Parabolic(R, x, constraints, temp);
-                break;
-            case PF::I_BARRIER : return InfiniteBarrier(R, x, constraints);
-                break;
-            case PF::LOG : return Log(R, x, constraints);
-                break;
-            case PF::INVERSE : return Inverse(R, x, constraints);
-                break;
-            case PF::BRACKET : return Bracket(R, x, constraints, temp);
-                break;
-            default : return Bracket(R, x, constraints, temp);
-
-        }
-    }
-
-    double GetMultiplierPenalty(double R, Eigen::VectorXd x, std::vector<std::function<double (Eigen::VectorXd)>> eq_const,
-                                std::vector<std::function<double (Eigen::VectorXd)>> ineq_const, Eigen::VectorXd tau, Eigen::VectorXd sigma) {
+    double GetMultiplierPenalty(std::vector<std::function<double (Eigen::VectorXd)>> eq_const,
+                                std::vector<std::function<double (Eigen::VectorXd)>> ineq_const, 
+				Eigen::VectorXd x, Eigen::VectorXd tau, Eigen::VectorXd sigma) {
         
         double sub = 0;
         for(int i = 0; i<ineq_const.size(); i++){
@@ -891,47 +882,38 @@ namespace Optimizer {
             sub -= pow(tau(i), 2);
         }
 
-        return Bracket(R, x, ineq_const, sigma) + Parabolic(R, x, eq_const, tau) + R * sub;
+        return Bracket(ineq_const, x, sigma) + Parabolic(eq_const, x, tau) + 0.1 * sub;
     }
     
-    Eigen::VectorXd PenaltyConstrained(std::function<double (Eigen::VectorXd)> obj_func, Eigen::VectorXd x, 
-                                        std::vector<std::function<double (Eigen::VectorXd)>> eq_const,
-                                       std::vector<std::function<double (Eigen::VectorXd)>> ineq_const, double Req, 
-                                       double ceq, double Rineq, double cineq,
-                                       PF p_func_eq = PF::BRACKET, PF p_func_ineq = PF::PARABOLIC, int M = 100){
+	Eigen::VectorXd PenaltyConstrained (std::function<double (Eigen::VectorXd)> obj_func, Eigen::VectorXd x,
+			std::vector<std::function<double (Eigen::VectorXd)>> ineq, std::vector<std::function<double (Eigen::VectorXd)>> eq,
+			int M = 100, double Rineq = 0.1, double Req = 0.1, double cineq = 10, double ceq = 10,
+			PF Pineq = PF::BRACKET, PF Peq = PF::PARABOLIC, double epsilon = 1e-5) {
 
-        double f_new, f;
-        Eigen::VectorXd x_new = x;
+		double f_prev, f;
 
-        for(int i=0; i<M; ++i){
+		int it = 0;
+		for (it = 0; it < M; ++it) {
+			std::function<double (Eigen::VectorXd)> func = [obj_func, ineq, eq, Rineq, Req, Pineq, Peq]
+				(Eigen::VectorXd x)->double { return obj_func(x) + Rineq * GetPenalty(ineq, x, Pineq) +
+				Req * GetPenalty(eq, x, Peq); };
 
-            std::function<double (Eigen::VectorXd)> func = [Req, Rineq, p_func_eq, p_func_ineq, obj_func, x, ineq_const, eq_const] (Eigen::VectorXd x)->double { 
-                return obj_func(x) + GetPenalty(Req, x, eq_const, p_func_eq) + GetPenalty(Rineq, x, ineq_const, p_func_ineq); };
+			if (it > 0)
+				f_prev = f;
 
-            if(i != 0) {
-                f = f_new;
-                x = x_new;
-            }
+			x = MVOptimize(func, x, 150);
+			f = func(x);
 
-            x_new = MVOptimize(func, x);
-            f_new = func(x_new);
+			if (it > 0 && abs(f - f_prev) < epsilon)
+				break;
 
-            if(i != 0 && std::abs(f_new - f) <= 1e-5)
-                break;
-            else {
-                Req *= ceq;
-                Rineq *= cineq;
-            }
+			Rineq *= cineq;
+			Req *= ceq;
+		}
 
+		return x;
+	}
 
-
-        }
-
-        return x_new;
-
-    }
-
-    // TODO
     Eigen::VectorXd MultiplierConstrained(std::function<double (Eigen::VectorXd)> obj_func, Eigen::VectorXd x, 
                                             std::vector<std::function<double (Eigen::VectorXd)>> eq_const,
                                             std::vector<std::function<double (Eigen::VectorXd)>> ineq_const, double R, int M = 100){
@@ -944,7 +926,7 @@ namespace Optimizer {
         for(int i=0; i<M; ++i){
 
             std::function<double (Eigen::VectorXd)> func = [R, obj_func, x, ineq_const, eq_const, sigma, tau] (Eigen::VectorXd x)->double { 
-                return obj_func(x) + GetMultiplierPenalty(R, x, eq_const, ineq_const, tau, sigma); };
+                return obj_func(x) + GetMultiplierPenalty(eq_const, ineq_const, x, tau, sigma); };
 
             if(i != 0) {
                 f = f_new;
@@ -979,48 +961,6 @@ namespace Optimizer {
         return x_new;
 
     }
-    
-=======
-	double GetPenalty (std::vector<std::function<double (Eigen::VectorXd)>> constraints, Eigen::VectorXd x, PF penalty = PF::BRACKET) {
-		switch (penalty) {
-			case PF::BRACKET: return Bracket(constraints, x);
-			case PF::PARABOLIC: return Parabolic(constraints, x);
-			case PF::LOG: return Log(constraints, x);
-			case PF::INVERSE: return Inverse(constraints, x);
-			case PF::INF_BARRIER: return InfiniteBarrier(constraints, x);
-			default: return Bracket(constraints, x);
-		}
-	}
-
-	Eigen::VectorXd PenaltyConstrained (std::function<double (Eigen::VectorXd)> obj_func, Eigen::VectorXd x,
-			std::vector<std::function<double (Eigen::VectorXd)>> ineq, std::vector<std::function<double (Eigen::VectorXd)>> eq,
-			int M = 100, double Rineq = 0.1, double Req = 0.1, double cineq = 10, double ceq = 10,
-			PF Pineq = PF::BRACKET, PF Peq = PF::PARABOLIC, double epsilon = 1e-5) {
-
-		double f_prev, f;
-
-		int it = 0;
-		for (it = 0; it < M; ++it) {
-			std::function<double (Eigen::VectorXd)> func = [obj_func, ineq, eq, Rineq, Req, Pineq, Peq]
-				(Eigen::VectorXd x)->double { return obj_func(x) + Rineq * GetPenalty(ineq, x, Pineq) +
-				Req * GetPenalty(eq, x, Peq); };
-
-			if (it > 0)
-				f_prev = f;
-
-			x = MVOptimize(func, x, 150);
-			f = func(x);
-
-			if (it > 0 && abs(f - f_prev) < epsilon)
-				break;
-
-			Rineq *= cineq;
-			Req *= ceq;
-		}
-
-		return x;
-	}
->>>>>>> master
 
 }
 
