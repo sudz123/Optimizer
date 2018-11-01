3054ad3166d0550a5547406737c89d70679e41d6
vinnyA3 <vincent.aceto@gmail.com>
2018-11-01 18:10:48 -0400

    feat(src): remodel dir structure

--- Documentation/src/js/main.js
+++ Documentation/src/js/main.js
@@ -1,6 +1,5 @@
-import './styles/main.scss';
+import overview from '../templates/overview';
 import marked from 'marked';
-import overview from './templates/overview';
 
 const mainContent = document.getElementById('content');
 mainContent.innerHTML = overview;
